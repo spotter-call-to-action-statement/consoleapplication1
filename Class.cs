ad96476890307a31586990052bf60129f916bb5f
spotter-call-to-action-statement <spotter_call_to_action_statement@lycos.com>
2016-12-29 19:46:23 -0600

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ConsoleApplication1
+{
+	/// <summary>
+	/// Summary description for Class.
+	/// </summary>
+	class Class
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			//
+			// TODO: Add code to start application here
+			//
+		}
+	}
+}
